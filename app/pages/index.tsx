a23a326cbce756e9cdece09fe619758101c9b55e
kevin <kevindbaik@gmail.com>
2024-04-18 15:49:11 -0700

    initial commit

---
+++ app/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Welcome to My Website</h1>
+        <p>This is the homepage of my Next.js project.</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
